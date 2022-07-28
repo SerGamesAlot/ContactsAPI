4e9dd5b0f405ef31a8b02e88236b93ca0197a7a9
Daniel Stoop <dannystoop19@gmail.com>
2022-07-28 11:19:45 +0200

    Updated Database datatypes

---
+++ ContactsAPI/Migrations/20220728091911_DatatypesAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsAPI.Migrations
+{
+    public partial class DatatypesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Contacts",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Contacts",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
--- ContactsAPI/Models/Contact.cs
+++ ContactsAPI/Models/Contact.cs
@@ -1,11 +1,22 @@
-﻿namespace ContactsAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace ContactsAPI.Models
 {
     public class Contact
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Full Name Is Too Long")]
         public string FullName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public long Phone { get; set; }
+        [Required]
+        [StringLength(maximumLength: 250, ErrorMessage = "Address Is Too Long")]
         public string Address { get; set; }
     }
 }
